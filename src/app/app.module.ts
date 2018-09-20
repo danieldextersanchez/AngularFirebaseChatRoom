6c5b7d668030d9b702ec9ffc835f77ad7c117ac3
Daniel Sanchez <danieldextersanchez@gmail.com>
2018-09-21 01:26:36 +0800

    basic retrieval of data from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+//Angular Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { RoomComponent } from './room/room.component';
+
+var firebaseconfig = {
+  apiKey: "AIzaSyBgAeG2uvRMSSB7FnKfPGTZNogrtuAOU74",
+  authDomain: "chatroom-f2b0f.firebaseapp.com",
+  databaseURL: "https://chatroom-f2b0f.firebaseio.com",
+  projectId: "chatroom-f2b0f",
+  storageBucket: "chatroom-f2b0f.appspot.com",
+  messagingSenderId: "622730052233"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseconfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/room/room.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore'; 
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+interface Message{
+  username : string,
+  message : string,
+  date : Date,
+  room : number
+}
+
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
+})
+export class RoomComponent implements OnInit {
+
+  MessageCollection: AngularFirestoreCollection<Message>
+  messages: Observable<Message[]>
+  
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit() {
+    this.MessageCollection = this.afs.collection('messages');
+    this.messages = this.MessageCollection.valueChanges();
+  }
+
+}
